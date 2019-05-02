30f65e2ff53cd3e7f72e59b9703cc8531b7f8453
Borna Novak <dosadnizub@gmail.com>
2019-05-02 13:38:08 +0200

    fixes for rails compatibility

--- lib/common/redlock_pooled_client.rb
+++ lib/common/redlock_pooled_client.rb
@@ -1,22 +1,24 @@
-class Redlock::PooledClient < Struct.new(:redis_connection_pool)
+if defined?(Redlock)
+  class Redlock::PooledClient < Struct.new(:redis_connection_pool)
 
-  def lock *args, &block
-    with_client{ |client| client.lock(*args, &block) }
-  end
-
-  def unlock *args, &block
-    with_client{ |client| client.unlock(*args, &block) }
-  end
-
-  private
-
-  def with_client
-    rv = nil
-    redis_connection_pool.with do |redis|
-      client = Redlock::Client.new([redis])
-      rv = yield client
+    def lock *args, &block
+      with_client{ |client| client.lock(*args, &block) }
     end
-    rv
-  end
 
+    def unlock *args, &block
+      with_client{ |client| client.unlock(*args, &block) }
+    end
+
+    private
+
+    def with_client
+      rv = nil
+      redis_connection_pool.with do |redis|
+        client = Redlock::Client.new([redis])
+        rv = yield client
+      end
+      rv
+    end
+
+  end
 end
--- lib/common/sequel_model/resolvable.rb
+++ lib/common/sequel_model/resolvable.rb
@@ -1,42 +1,44 @@
-class Sequel::Model
-  module Resolvable
-    extend ActiveSupport::Concern
+if defined?(Sequel::Model)
+  class Sequel::Model
+    module Resolvable
+      extend ActiveSupport::Concern
 
-    class Unresolvable < StandardError; end
+      class Unresolvable < StandardError; end
 
-    included do
-      dataset_module do
-        def resolve obj, options = {}
-          if obj.class == model
-            rv = obj
-          elsif obj.class == String
-            rv = where(logid: obj).first
-          elsif obj.class == Integer
-            rv = where(id: obj).first
-          end
-          if !rv && options[:raise]
-            raise Unresolvable.new("can't resolve #{obj.class} #{obj.inspect}")
-          end
+      included do
+        dataset_module do
+          def resolve obj, options = {}
+            if obj.class == model
+              rv = obj
+            elsif obj.class == String
+              rv = where(logid: obj).first
+            elsif obj.class == Integer
+              rv = where(id: obj).first
+            end
+            if !rv && options[:raise]
+              raise Unresolvable.new("can't resolve #{obj.class} #{obj.inspect}")
+            end
 
-          rv
-        end
-
-        def resolve_id obj, options = {}
-          if obj.class == model
-            rv = obj.id
-          elsif obj.class == String
-            rv = where(logid: obj).pluck(:id).first
-          elsif obj.class == Integer
-            rv = id
-          end
-          if !rv && options[:raise]
-            raise Unresolvable.new("can't resolve #{obj.class} #{obj.inspect}")
+            rv
           end
 
-          rv
+          def resolve_id obj, options = {}
+            if obj.class == model
+              rv = obj.id
+            elsif obj.class == String
+              rv = where(logid: obj).pluck(:id).first
+            elsif obj.class == Integer
+              rv = id
+            end
+            if !rv && options[:raise]
+              raise Unresolvable.new("can't resolve #{obj.class} #{obj.inspect}")
+            end
+
+            rv
+          end
         end
       end
-    end
 
+    end
   end
 end
