454eb710faa2acf0dd0abfb728d03b9e7bd7f9f1
Borna Novak <dosadnizub@gmail.com>
2020-07-02 21:02:43 +0200

    fixed a nasty nondeterministic cache bug involving Redis#multi

--- lib/common/cache.rb
+++ lib/common/cache.rb
@@ -7,16 +7,15 @@ class Cache
   def initialize redis_connection_pool, options = {}
     @redis = redis_connection_pool
     @namespace = options.delete(:namespace) || 'cache'
+    @namespace.freeze
   end
 
   def fetch key, options = {}
     value = @redis.with{ |redis| redis.get(k(key)) }
 
     if value
-      # InfoCoin.log.debug "cache hit for #{key}"
       value
     else
-      # InfoCoin.log.debug "cache miss for #{key}"
       if block_given?
         value = yield
         write key, value, options
@@ -30,10 +29,16 @@ class Cache
     expire_in = options.delete(:expire_in) || DEFAULT_EXPIRE_IN
 
     @redis.with do |redis|
-      redis.multi do |redis|
-        redis.set(k(key), value)
-        redis.expire(k(key), expire_in.to_i)
-      end
+      idx = 0
+      begin
+        warn "Cache#write repeating storing key '#{key}' #{idx} times" if idx > 1
+        raise "Cache#write can't write '#{key}' for #{idx} times!" if idx >= 10
+        redis.multi do |redis|
+          redis.set(k(key), value)
+          redis.expire(k(key), expire_in.to_i)
+        end
+        idx += 1
+      end until redis.exists(k(key))
     end
 
     value
