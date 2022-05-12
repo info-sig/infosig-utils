83646d4aa22deda934ed95a84fa045f1348afe17
kauzlaricmarko <kauzlaricmarko252@gmail.com>
2022-05-12 13:52:09 +0200

    add retryer and tests

--- lib/common/retryer.rb
+++ lib/common/retryer.rb
@@ -1,1 +1,34 @@
+class Retryer
+  extend ClassFunctional
 
+  def self.call options = {}, &block
+    repeats = options[:repeats] || 3
+    sleeps  = options[:sleeps]  || 0.seconds
+
+    count = 0
+    rv = {}
+    while count < repeats
+      rv = retry_once(&block)
+      return rv[:rv] if rv[:status] == 'OK'
+      count = count + 1
+      warn "#{rv[:exception].class}: #{rv[:exception].message}, retry #{count} of #{repeats}"
+      sleep sleeps
+    end
+    raise rv[:exception]
+  end
+
+
+  private
+
+  def self.retry_once &block
+    {
+      rv: block.call,
+      status: 'OK'
+    }
+  rescue Exception => e
+    {
+      exception: e,
+      status: 'error'
+    }
+  end
+end
